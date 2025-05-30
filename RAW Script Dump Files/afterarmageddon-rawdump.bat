07d0435ba942ca269e7f64b8181a4ec7571a63d9
BlackPaladin2022 <99116758+BlackPaladin2022@users.noreply.github.com>
2025-05-30 16:23:02 -0400

    Add files via upload

---
+++ RAW Script Dump Files/afterarmageddon-rawdump.bat
@@ -0,0 +1,107 @@
+cartographer ancient.grp ancient_commands.txt AAG_ANCIENT.GRP_script -m
+
+pause
+
+cartographer atlantic.grp atlantic_commands.txt AAG_ATLANTIC.GRP_script -m
+
+pause
+
+cartographer atlantis.grp atlantis_commands.txt AAG_ATLANTIS.GRP_script -m
+
+pause
+
+cartographer burycity.grp burycity_commands.txt AAG_BURYCITY.GRP_script -m
+
+pause
+
+cartographer castle.grp castle_commands.txt AAG_CASTLE.GRP_script -m
+
+pause
+
+cartographer delim.grp delim_commands.txt AAG_DELIM.GRP_script -m
+
+pause
+
+cartographer earth.grp earth_commands.txt AAG_EARTH.GRP_script -m
+
+pause
+
+cartographer faren.grp faren_commands.txt AAG_FAREN.GRP_script -m
+
+pause
+
+cartographer field1.grp field1_commands.txt AAG_FIELD1.GRP_script -m
+
+pause
+
+cartographer field2.grp field2_commands.txt AAG_FIELD2.GRP_script -m
+
+pause
+
+cartographer jupiter.grp jupiter_commands.txt AAG_JUPITER.GRP_script -m
+
+pause
+
+cartographer lamu.grp lamu_commands.txt AAG_LAMU.GRP_script -m
+
+pause
+
+cartographer mars.grp mars_commands.txt AAG_MARS.GRP_script -m
+
+pause
+
+cartographer mercury.grp mercury_commands.txt AAG_MERCURY.GRP_script -m
+
+pause
+
+cartographer moon.grp moon_commands.txt AAG_MOON.GRP_script -m
+
+pause
+
+cartographer mupalace.grp mupalace_commands.txt AAG_MUPALACE.GRP_script -m
+
+pause
+
+cartographer museum.grp museum_commands.txt AAG_MUSEUM.GRP_script -m
+
+pause
+
+cartographer neptune.grp neptune_commands.txt AAG_NEPTUNE.GRP_script -m
+
+pause
+
+cartographer plute.grp plute_commands.txt AAG_PLUTE.GRP_script -m
+
+pause
+
+cartographer room.grp room_commands.txt AAG_ROOM.GRP_script -m
+
+pause
+
+cartographer ruin.grp ruin_commands.txt AAG_RUIN.GRP_script -m
+
+pause
+
+cartographer saturn.grp saturn_commands.txt AAG_SATURN.GRP_script -m
+
+pause
+
+cartographer uranus.grp uranus_commands.txt AAG_URANUS.GRP_script -m
+
+pause
+
+cartographer venus.grp venus_commands.txt AAG_VENUS.GRP_script -m
+
+pause
+
+cartographer volcano.grp volcano_commands.txt AAG_VOLCANO.GRP_script -m
+
+pause
+
+cartographer initdata.bin initdata.bin_commands.txt AAG_INITDATA.BIN_script -m
+
+pause
+
+cartographer bat_data.bin bat_data.bin_commands.txt AAG_BAT_DATA.BIN_script -m
+
+pause
